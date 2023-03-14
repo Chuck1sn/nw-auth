8d8d37c4195b82d7b5ab50f932598bd9ae350e9e
Chuck Lin <chuck1inzl@gmail.com>
2023-03-14 14:59:55 +0800

    fix configuration

---
+++ core/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  rules: {
+  },
+  plugins: [
+    '@typescript-eslint'
+  ]
+}
