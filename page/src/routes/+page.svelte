0d3d39b2d9a560f35ed1da8d7796f383c90089da
Chuck Lin <chuck1inzl@gmail.com>
2023-03-14 21:16:40 +0800

    form

--- page/src/routes/+page.svelte
+++ page/src/routes/+page.svelte
@@ -1,19 +1,22 @@
-<h1>Select Platform</h1>
-<div>
-	<select name="pets" id="pet-select">
-		<option value="">--Please choose an option--</option>
-		<option value="dog">Dog</option>
-		<option value="cat">Cat</option>
-		<option value="hamster">Hamster</option>
-		<option value="parrot">Parrot</option>
-		<option value="spider">Spider</option>
-		<option value="goldfish">Goldfish</option>
-	</select>
-</div>
-
-<div>
-	<p><label>appId</label><br /><input /></p>
-	<p><label>appSecret</label><br /><input /></p>
-	<p><label>appTicket</label><br /><input /></p>
-	<p><label>callback</label><br /><input /></p>
-</div>
+<h1>Platform Server Info</h1>
+<form action="" method="post">
+	<p>
+		<select name="pets" id="pet-select">
+			<option value="">--Please choose an option--</option>
+			<option value="dog">Dog</option>
+			<option value="cat">Cat</option>
+			<option value="hamster">Hamster</option>
+			<option value="parrot">Parrot</option>
+			<option value="spider">Spider</option>
+			<option value="goldfish">Goldfish</option>
+		</select>
+	</p>
+	<fieldset>
+		<legend>Oidc Info</legend>
+		<p><label>appId:<input type="text" name="appId" required /></label></p>
+		<p><label>appSecret:<input type="text" name="appSecret" required /></label></p>
+		<p><label>appTicket:<input type="text" name="appTicket" required /></label></p>
+		<p><label>callback:<input type="url" name="callback" required /></label></p>
+		<p><input type="submit" value="submit" /></p>
+	</fieldset>
+</form>
