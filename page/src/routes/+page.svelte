21ab51da2e020d49311c706b72c4ddb1afa8b6c3
Chuck Lin <chuck1inzl@gmail.com>
2023-03-15 16:55:02 +0800

    improve page style

--- page/src/routes/+page.svelte
+++ page/src/routes/+page.svelte
@@ -1,8 +1,8 @@
-<h1>Platform Server Info</h1>
 <form action="" method="post">
+	<h1>Platform Server Info</h1>
 	<p>
 		<select name="pets" id="pet-select">
-			<option value="">--Please choose an option--</option>
+			<option value="">Choose a platform </option>
 			<option value="dog">Dog</option>
 			<option value="cat">Cat</option>
 			<option value="hamster">Hamster</option>
@@ -20,3 +20,46 @@
 		<p><input type="submit" value="submit" /></p>
 	</fieldset>
 </form>
+
+<style>
+	form {
+		display: flex;
+		font-size: 1.5em;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	input {
+		font-size: 1em;
+		margin-left: 0.5em;
+	}
+	input[type='submit'] {
+		background-color: rgb(123, 122, 122);
+		color: white;
+		border: outset;
+		cursor: pointer;
+		margin-left: 0em;
+	}
+	input[type='submit']:hover {
+		background-color: rgb(150, 149, 149);
+	}
+	select {
+		appearance: none;
+		font-size: 1em;
+		padding: 0.5em;
+		background-color: #eee;
+		border: none;
+		border-radius: 0.25em;
+		box-shadow: inset 0 0 0.5em rgba(0, 0, 0, 0.2);
+		cursor: pointer;
+	}
+	select::-ms-expand {
+		display: none;
+	}
+	select option {
+		background-color: white;
+	}
+	fieldset {
+		box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+	}
+</style>
---
+++ page/src/routes/userinfo/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Platform User Info</h1>
+<div>
+	<pre>
+formatted json 
+  </pre>
+</div>
