eb93f4fe242c62d046442e5b8a56a5c7b18d4cda
Chuck Lin <chuck1inzl@gmail.com>
2023-03-16 14:24:31 +0800

    add user-info page

---
+++ page/src/routes/user-info/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	let data = {
+		name: 'John Doe',
+		age: 30,
+		city: 'New York'
+	}
+	let copyText = 'Copy to Clipboard'
+	let copyToClipboard = () => {
+		let jsonData = JSON.stringify(data, null, 2)
+		navigator.clipboard.writeText(jsonData)
+		copyText = 'copied!'
+	}
+</script>
+
+<div>
+	<h1>UserInfo</h1>
+	<button class="copy-btn" on:click={copyToClipboard}>
+		<span>{copyText}</span>
+	</button>
+	<pre>{JSON.stringify(data, null, 2)}</pre>
+</div>
+
+<style>
+	div {
+		display: flex;
+		font-size: 1.5em;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.copy-btn {
+		border: outset;
+	}
+	.copy-btn:active {
+		border: inset;
+	}
+</style>
--- page/src/routes/userinfo/+page.svelte
+++
@@ -1,6 +0,0 @@
-<h1>Platform User Info</h1>
-<div>
-	<pre>
-formatted json 
-  </pre>
-</div>
