ad3ad66aab127002f968df8b775aca1a1f077f7e
Chuck Lin <chuck1inzl@gmail.com>
2023-10-01 10:46:21 +0800

    add error page

---
+++ page/src/errors/Error.svelte
@@ -0,0 +1,29 @@
+<template>
+  <div class="error-page">
+    <h1>Oops! Something went wrong.</h1>
+    <p>We're sorry, but an unexpected error occurred.</p>
+    <p>Please try again later.</p>
+  </div>
+</template>
+
+<style>
+  .error-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    font-family: Arial, sans-serif;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+  }
+</style>
